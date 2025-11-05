1df22928e5f64e56392f94964b13f5da3ff88a08
Kaam-umcg <a.t.van.kaam@umcg.nl>
2025-11-05 16:05:33 +0100

    better benchmark plots

--- benchmark/plot_benchmark.R
+++ benchmark/plot_benchmark.R
@@ -1,10 +1,22 @@
 library(CINsim)
 library(ggplot2)
+library(ggpubr)
 library(dplyr)
 
 # hardcoded paths because I love making this someone else their problem I suppose
 base_data_path <- "~/CINsim_plots/benchmark/results"
 base_plot_path <- "~/CINsim_plots/benchmark/plots"
+
+cb_palette <- c(
+  "#E69F00", # orange
+  "#56B4E9", # sky blue
+  "#009E73", # bluish green
+  "#F0E442", # yellow 
+  "#0072B2", # blue
+  "#D55E00", # vermillion
+  "#CC79A7", # reddish purple
+  "#999999"  # grey
+)
 
 if (!(dir.exists(base_plot_path))){
     dir.create(base_plot_path)
@@ -30,6 +42,9 @@ for(var in benchmark_vars){
   # gets the param name from the dataframe - not consistent with var
   param <- setdiff(colnames(merge_frame), c("run", "cores", "time_taken"))[1]
   
+  print(merge_frame)
+  saveRDS(merge_frame, file.path(data_folder, "merged.Rds"))
+
   # summarises for plotting with ggplot
   df_summary <- merge_frame %>%
     group_by(cores, !!sym(param)) %>%
@@ -49,11 +64,28 @@ for(var in benchmark_vars){
       color = "Cores",
       title = "Simulation Time by Iterations and Number of Cores"
     ) +
-    theme_minimal()
-  
+    theme_minimal() +
+    scale_color_manual(values = cb_palette)
+
+  # q <- ggline(
+  #   data = merge_frame,
+  #   x = param,
+  #   y = "time_taken",
+  #   group = "cores",
+  #   color = "cores",
+  #   add = "mean_se",
+  #   error.plot = "errorbar",
+  #   numeric.x.axis = TRUE,
+  #   palette = "jco"   # or "aaas", "npg", "lancet", etc.
+  # )
+
   # sets the saving path for the plot
-  plot_fn <- file.path(base_plot_path, paste0(param ,".pdf"))
+  plot_fn <- file.path(base_plot_path, paste0(param, ".pdf"))
   # saves the plot and then we move on to the next var in the next iteration of the loop
   ggsave(plot_fn, plot = p)
+
+  # then we also save the ggpubr version to make it better
+  # plot_fn <- file.path(base_plot_path, paste0(param, "_ggpubr", ".pdf"))  
+  # ggsave(plot_fn, plot = q)
 }
 
